b0f8f298004d05547cb70f1013852c7370de0eef
Carolina Oliveira <10094036640@estudante.ufjf.br>
2020-11-24 15:45:42 -0300

    Change adapter to show data

--- app/src/main/java/br/ufjf/dcc196/caroliveira/filmes/MovieAdapter.java
+++ app/src/main/java/br/ufjf/dcc196/caroliveira/filmes/MovieAdapter.java
@@ -1,7 +1,11 @@
 package br.ufjf.dcc196.caroliveira.filmes;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,22 +22,41 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
     @NonNull
     @Override
     public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context ctx = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(ctx);
+        View movieView = inflater.inflate(R.layout.movie_layout, parent, false);
+        MovieViewHolder viewHolder = new MovieViewHolder(movieView);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
-
+        holder.textViewTitle.setText(movies.get(position).getTitle());
+        holder.textViewYear.setText(movies.get(position).getYear());
+        holder.textViewStyle.setText(movies.get(position).getStyle());
+        holder.textViewDirector.setText("de " + movies.get(position).getDirector());
+        holder.textViewWatched.setText(movies.get(position).getWatched()? "Já assistido" : "Não asssistido");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movies.size();
     }
 
     public class MovieViewHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle;
+        private TextView textViewYear;
+        private TextView textViewStyle;
+        private TextView textViewDirector;
+        private TextView textViewWatched;
+
         public MovieViewHolder(@NonNull View itemView) {
             super(itemView);
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewYear = itemView.findViewById(R.id.textViewYear);
+            textViewStyle = itemView.findViewById(R.id.textViewStyle);
+            textViewDirector = itemView.findViewById(R.id.textViewDirector);
+            textViewWatched = itemView.findViewById(R.id.textViewWatched);
         }
     }
 }
