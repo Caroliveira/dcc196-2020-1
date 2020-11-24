91b0c783f6d7d55936a878eb491857f92ee4f8fd
Carolina Oliveira <10094036640@estudante.ufjf.br>
2020-11-24 14:36:39 -0300

    Add recyclerViewMovies

--- app/src/main/java/br/ufjf/dcc196/caroliveira/filmes/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/caroliveira/filmes/MainActivity.java
@@ -1,14 +1,18 @@
 package br.ufjf.dcc196.caroliveira.filmes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
     }
 }
