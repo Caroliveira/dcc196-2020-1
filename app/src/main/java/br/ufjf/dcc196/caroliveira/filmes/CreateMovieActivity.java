695bd8fddc53c0538cd3a3f9b4baf5bc931143b6
Carolina Oliveira <10094036640@estudante.ufjf.br>
2020-11-24 18:15:33 -0300

    create add movie layout

---
+++ app/src/main/java/br/ufjf/dcc196/caroliveira/filmes/CreateMovieActivity.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc196.caroliveira.filmes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_movie);
+    }
+}
